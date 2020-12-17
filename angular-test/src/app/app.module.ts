ddd90a77746cb6149cfcf073974df6aebf3164c8
Noorodin Kiani <n.kiani@outlook.com>
2020-12-17 09:55:58 +0330

    Create config file.

--- angular-test/src/app/app.module.ts
+++ angular-test/src/app/app.module.ts
@@ -1,27 +1,23 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-import { RouterModule } from '@angular/router';
-import { HttpClientModule } from '@angular/common/http'
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
+import { RouterModule } from "@angular/router";
+import { HttpClientModule } from "@angular/common/http";
 
-import { AppComponent } from './app.component';
-import { PostsComponent } from './pages/posts/posts.component';
-import { PostComponent } from './components/post/post.component';
+import { AppComponent } from "./app.component";
+import { PostsComponent } from "./pages/posts/posts.component";
+import { PostComponent } from "./components/post/post.component";
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    PostsComponent,
-    PostComponent,
-  ],
+  declarations: [AppComponent, PostsComponent, PostComponent],
   imports: [
     BrowserModule,
     HttpClientModule,
     RouterModule.forRoot([
-      {path: 'posts', component: PostsComponent},
-      {path: '**', component: PostsComponent}
+      { path: "posts", component: PostsComponent },
+      { path: "**", component: PostsComponent },
     ]),
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
--- angular-test/src/app/pages/posts/posts.component.ts
+++ angular-test/src/app/pages/posts/posts.component.ts
@@ -1,23 +1,24 @@
-import { HttpClient } from '@angular/common/http';
-import { Component, OnInit } from '@angular/core';
+import { HttpClient } from "@angular/common/http";
+import { Component, OnInit } from "@angular/core";
+import { config } from "../../config";
 
 @Component({
-  selector: 'app-posts',
-  templateUrl: './posts.component.html',
-  styleUrls: ['./posts.component.scss']
+  selector: "app-posts",
+  templateUrl: "./posts.component.html",
+  styleUrls: ["./posts.component.scss"],
 })
 export class PostsComponent implements OnInit {
-  posts:any;
+  posts: any;
 
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient) {}
 
   ngOnInit() {
     this.getPosts();
   }
 
   getPosts() {
-    return this.http.get('http://localhost:3000/posts').subscribe(response=>{
-      this.posts=response;
+    return this.http.get(`${config.baseUrl}posts`).subscribe((response) => {
+      this.posts = response;
     });
   }
 }
